79317f4983d87c0fb7939899c5fa8ab36f1480d8
prachishahpss <106387955+prachishahpss@users.noreply.github.com>
2022-06-08 19:58:39 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,8 +54,8 @@ def get_fruit_load_list():
 # Add a button to load the fruit
 if streamlit.button('Get Fruit Load List'):
    
-      my_data_rows = get_fruit_load_list()
-      streamlit.dataframe(my_data_rows)
+my_data_rows = get_fruit_load_list()
+streamlit.dataframe(my_data_rows)
 
       
 #don't run anything past here while we troubleshoot
